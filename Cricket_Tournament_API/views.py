ea1d35a99d130af2c34a35c0f08af68a410f9ba0
Ajay Ankush Kharatmol <51756738+Ajaykharatmol@users.noreply.github.com>
2021-07-23 19:00:35 +0530

    Update views.py

--- Cricket_Tournament_API/views.py
+++ Cricket_Tournament_API/views.py
@@ -295,3 +295,32 @@ class Tournament_ScoreDetail(APIView):
 
 
 
+    
+    
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+#from .models import Books
+#from .forms import BooksForm
+from django.template import Context
+from django.shortcuts import render
+import requests
+
+#def index(request):
+    #emp = Books.objects.all()
+    #return render(request,'dashboard.html')
+
+def index(request):
+    res = requests.get('https://cricketleaguetournament.herokuapp.com/Cricket_Tournament_API/countries/')
+
+    response_data = res.json()
+    
+    #print(response_data)
+    products = response_data[0]
+    print(products)
+   
+    if res.status_code == 200:
+        return render(request, 'dashboard.html',
+                      {"products": products})
+    else:
+        print("error")
